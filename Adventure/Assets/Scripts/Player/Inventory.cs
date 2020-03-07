062c11910c7a1d72799bfe619cdbad4e6c1b09c4
Sloan Kelly <sloan@codehoose.com>
2020-03-07 15:07:24 -0500

    Player movement and object pickup

---
+++ Adventure/Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private Pickup _currentItem;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Pickup")
+        {
+            var newItem = collision.gameObject.GetComponent<Pickup>();
+            if (newItem == _currentItem)
+            {
+                return;
+            }
+            else
+            {
+                PickUp(newItem);
+            }
+        }
+    }
+
+    public void PickUp(Pickup pickup)
+    {
+        if (_currentItem == pickup)
+        {
+            return;
+        }
+
+        if (_currentItem != null)
+        {
+            _currentItem.gameObject.transform.SetParent(null);
+        }
+
+        _currentItem = pickup;
+        _currentItem.gameObject.transform.SetParent(transform);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && _currentItem != null)
+        {
+            _currentItem.gameObject.transform.SetParent(null);
+            _currentItem = null;
+        }
+    }
+}
---
+++ Adventure/Assets/Scripts/Player/Locomotion.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Locomotion : MonoBehaviour
+{
+    [Header("Locomotion Variables")]
+    public float _speed = 20;
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+        var y = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+
+        if (Mathf.Abs(x) > 0)
+        {
+            transform.position += new Vector3(x, 0);
+        }
+
+        if (Mathf.Abs(y) > 0)
+        {
+            transform.position += new Vector3(0, y);
+        }
+    }
+}
