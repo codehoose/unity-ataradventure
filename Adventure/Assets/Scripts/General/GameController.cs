9fdad00af88a38a995df2d8323c64e5f4e64cf6d
Sloan Kelly <sloan@codehoose.com>
2020-03-14 13:23:43 -0400

    Player screen bounds detection and response

---
+++ Adventure/Assets/Scripts/General/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private BoxCollider2D _collider;
+    private PlayerBoundsDetect _playerBoundsDetect;
+    public Transform _player;
+
+    private void Awake()
+    {
+        _collider = GetComponent<BoxCollider2D>();
+        _playerBoundsDetect = new PlayerBoundsDetect(_collider, _player);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _playerBoundsDetect.HandleEvent(collision);
+    }
+}
---
+++ Adventure/Assets/Scripts/General/Helpers/PlayerBoundsDetect.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+class PlayerBoundsDetect
+{
+    private readonly BoxCollider2D _collider;
+    private readonly Transform _player;
+
+    public PlayerBoundsDetect(BoxCollider2D collider, Transform player)
+    {
+        _collider = collider;
+        _player = player;
+    }
+
+    public void HandleEvent(Collider2D collision)
+    {
+        if (collision.tag != "Player")
+        {
+            return;
+        }
+
+        var horizontal = Vector3.Cross(Vector3.up, collision.transform.position.normalized);
+        if (Mathf.Abs(horizontal.z) > 0.88f)
+        {
+            var direction = Mathf.Sign(horizontal.z);
+            var offset = _collider.bounds.extents.x * 2f * direction;
+            _player.position += new Vector3(offset, 0);
+            return;
+        }
+
+        var vertical = Vector3.Cross(Vector3.right, collision.transform.position.normalized);
+        if (Mathf.Abs(vertical.z) > 0f)
+        {
+            var direction = -Mathf.Sign(vertical.z);
+            var offset = _collider.bounds.extents.y * 2f * direction;
+            _player.position += new Vector3(0, offset);
+        }
+    }
+}
